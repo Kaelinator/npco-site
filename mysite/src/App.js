16a18d3e9e396a20c507b44dcf6a3bf5e7543693
Kaelinator <kirkkael@gmail.com>
2018-06-08 13:58:06 -0500

    I made some example changes

---
+++ mysite/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={'https://proxy.duckduckgo.com/iur/?f=1&image_host=http%3A%2F%2Fwww.theassassingame.com%2Fuserdata%2Fnewsfeed_pics%2Fla6iH0nBh8ZWy1e%2FJoe%2520Cusack.jpg&u=https://www.theassassingame.com/userdata/newsfeed_pics/la6iH0nBh8ZWy1e/Joe%20Cusack.jpg'} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          This nutty!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
